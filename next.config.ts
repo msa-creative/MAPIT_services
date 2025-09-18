76342c99b32e0e956d2433f453b7c4fb5e96af32
msa-creative <fixtrue7@gmail.com>
2025-09-18 14:15:07 +0500

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 import path from "node:path";
 
+const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
 
 const nextConfig: NextConfig = {
   images: {
@@ -16,6 +17,13 @@ const nextConfig: NextConfig = {
     ],
   },
   outputFileTracingRoot: path.resolve(__dirname, '../../'),
+  turbopack: {
+    rules: {
+      "*.{jsx,tsx}": {
+        loaders: [LOADER]
+      }
+    }
+  }
 };
 
 export default nextConfig;
